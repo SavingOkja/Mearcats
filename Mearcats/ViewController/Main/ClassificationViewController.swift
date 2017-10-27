ce6a51a980a9174cb280bbc72f4c8a984eae6a54
EJSohn <5on.3un.7oo@gmail.com>
2017-10-28 01:52:13 +0900

    [ADD] Mapping ClassificationViewController with storyboard object.

---
+++ Mearcats/ViewController/Main/ClassificationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ClassificationViewController.swift
+//  Mearcats
+//
+//  Created by 손은주 on 2017. 10. 28..
+//  Copyright © 2017년 SavingOkja. All rights reserved.
+//
+
+import UIKit
+
+class ClassificationViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
