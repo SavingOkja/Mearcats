26f2f736fe0b6e11f19217616d305846f439d890
EJSohn <5on.3un.7oo@gmail.com>
2017-10-28 01:26:16 +0900

    [ADD] MainPageViewController for dot pager control

---
+++ Mearcats/ViewController/MainPageViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainPageViewController.swift
+//  Mearcats
+//
+//  Created by 손은주 on 2017. 10. 28..
+//  Copyright © 2017년 SavingOkja. All rights reserved.
+//
+
+import UIKit
+
+class MainPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+}
