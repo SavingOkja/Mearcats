d308a2a7526ad28eb15aa52e3d197eb538533a7a
EJSohn <5on.3un.7oo@gmail.com>
2017-10-29 00:27:01 +0900

    [ADD] add TableView

---
+++ Mearcats/Model/Token.swift
@@ -0,0 +1,26 @@
+//
+//  Token.swift
+//  Mearcats
+//
+//  Created by 손은주 on 2017. 10. 28..
+//  Copyright © 2017년 SavingOkja. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class TokenResult: Mappable {
+    var result: Bool = false
+    var msg: String = ""
+    var token: String = ""
+    
+    required convenience init?(map: Map) {
+        self.init()
+    }
+    
+    func mapping(map: Map) {
+        result <- map["result"]
+        msg <- map["msg"]
+        token <- map["data"]
+    }
+}
