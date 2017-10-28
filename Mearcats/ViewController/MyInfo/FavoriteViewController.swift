66ecf91f7ded3666041578314db796dcb4c81421
EJSohn <5on.3un.7oo@gmail.com>
2017-10-28 13:54:47 +0900

    [ADD] Set FavoriteViewController to is's collectionView's delegate& datasource

--- Mearcats/ViewController/MyInfo/FavoriteViewController.swift
+++ Mearcats/ViewController/MyInfo/FavoriteViewController.swift
@@ -9,16 +9,42 @@
 import UIKit
 import XLPagerTabStrip
 
-class FavoriteViewController: UIViewController, IndicatorInfoProvider {
+class FavoriteViewController: UIViewController,
+                              IndicatorInfoProvider,
+                              UICollectionViewDelegate,
+                              UICollectionViewDataSource {
 
-    
+    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 
     func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
         return "Favorite"
     }
+    
+    /// UICollectionView Delegate Methods.
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FavoriteCollectionViewCell", for: indexPath) as! FavoriteCollectionViewCell
+        return cell
+    }
+}
+
+class FavoriteCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var companyLabel: UILabel!
+    @IBOutlet weak var followersLabel: UILabel!
 }
